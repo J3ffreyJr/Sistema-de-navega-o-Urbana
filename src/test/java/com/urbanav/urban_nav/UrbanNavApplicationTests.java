4e3c1a87c73634bd7215c6b4396652f6a535ed42
Jeffrey_Jr. <jeffwilljr05@gmail.com>
2026-06-06 23:30:25 +0200

    Update project files

---
+++ src/test/java/com/urbanav/urban_nav/UrbanNavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanav.urban_nav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanNavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
